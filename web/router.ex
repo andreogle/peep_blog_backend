9854456b5d45fa65205efbf7ac6d133d7d57ec5f
Andre Ogle <ogleandre@yahoo.com>
2015-06-08 14:39:01 +0200

    Initial commit

---
+++ web/router.ex
@@ -0,0 +1,29 @@
+defmodule PeepBlogBackend.Router do
+  use PeepBlogBackend.Web, :router
+
+  pipeline :browser do
+    plug :accepts, ["html"]
+    plug :fetch_session
+    plug :fetch_flash
+    plug :protect_from_forgery
+  end
+
+  pipeline :api do
+    plug :accepts, ["json"]
+    plug PlugCors, [origins: ["localhost:4200"]]
+  end
+
+  # scope "/", PeepBlogBackend do
+  #   pipe_through :browser # Use the default browser stack
+  #
+  #   get "/", PageController, :index
+  # end
+
+  # Other scopes may use custom stacks.
+  scope "/", PeepBlogBackend do
+    pipe_through :api
+
+    resources "/posts", PostController
+    options "/posts*anything", PostController, :options
+  end
+end
